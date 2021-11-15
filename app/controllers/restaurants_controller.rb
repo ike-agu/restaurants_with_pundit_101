4fc7f9d537d59ee20610a7291873ad54fc7a7359
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-11-15 18:03:09 +0000

    restaurant policy generated

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,6 +13,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
+    authorize @restaurant
   end
 
   # GET /restaurants/1/edit
